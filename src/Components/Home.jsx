8f700788bdd7aab35661e5229e54bfe378775ab8
khalifa-pendrops <dikee5200@gmail.com>
2025-03-26 09:52:25 +0100

    moving home text edited

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -83,7 +83,7 @@ const Home = () => {
     <div className=" d-flex flex-column justify-content-center align-items-center gap-5 py-5">
       <div className="container-fluid d-block">
         <div className="scrolling-text container-fluid d-flex flex-column justify-content-center textAlign-center ">
-          <div className="scrolling-text_2 p-2 lh-1 fs-2 ">  
+          <div className="scrolling-text_2 p-2 lh-1 fs-5 fs-sm-2 text-center">  
             <p>
               📢 On-going Project:{" "}
               <span className="fs-4 text-info">
